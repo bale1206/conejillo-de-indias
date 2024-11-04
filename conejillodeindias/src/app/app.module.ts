c0c3f106f44e184e0aee203f41e8648c612e20c3
cetecom_avaras <cetecom_avaras@duoc.cl>
2024-11-04 14:41:21 -0300

    afirmateeeee

--- conejillodeindias/src/app/app.module.ts
+++ conejillodeindias/src/app/app.module.ts
@@ -19,7 +19,19 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(),
+    IonicModule.forRoot(
+      {
+        rippleEffect:true,
+        animated: true,
+        mode: 'md',
+        hardwareBackButton: true
+        //ionic config//
+        //para llevar esto a android hay 3 comandos: ionic capacitor add android (agregar a mi proyecto la configuración de plataforma de android), 
+        //ionic capacitor sync (sincroniza el proyecto ionic con la version de android como utilidades, pluggins, librerias extras, etc), 
+        //ionic capacitor build android(compila y redirecciona hacia android studio con el proyecto ionic compilado por completo)
+        //para firmar la app 
+      }
+    ),
     AppRoutingModule,
     FormsModule,
     IonicStorageModule.forRoot(),
